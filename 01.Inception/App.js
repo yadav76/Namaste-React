4ecceb85357648e78dc13045140d2501e26809d2
santosh <yadavsantosh6720@gmail.com>
2024-01-20 14:37:05 +0530

    Inception

---
+++ 01.Inception/App.js
@@ -0,0 +1,22 @@
+/**
+ * <div class="parent">
+ *      <div class="child">
+ *           <h1>I am h1 Tag</h1>
+ *           <h2>I am h2 Tag</h2>
+ *      </div>
+ * </div>
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I am h1 Tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ])
+);
+
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
---
+++ 01.Inception/readme.md
@@ -0,0 +1,7 @@
+# Namaste React
+
+# Inception
+
+# In this Chapter I learned that we can configure React in our website by using CDN also. We don't need to install NPM always to use React.
+
+# I have also learned that I can implement React in small part of our website like in Header part, in Footer part Or inside the any Div also.
