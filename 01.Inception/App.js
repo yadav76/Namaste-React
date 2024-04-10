be3bb1fdf44fe659da4dfef085f2d2ab9bb6e109
yadav76 <93885474+yadav76@users.noreply.github.com>
2024-04-10 08:49:34 +0530

    commit

--- 01.Inception/App.js
+++ 01.Inception/App.js
@@ -7,14 +7,25 @@
  * </div>
  */
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+// const parent = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I am h1 Tag"),
+//     React.createElement("h2", {}, "I am h2 tag"),
+//   ])
+// );
+
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am h1 Tag"),
     React.createElement("h2", {}, "I am h2 tag"),
-  ])
-);
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am h1 Tag"),
+    React.createElement("h2", {}, "I am h2 tag"),
+  ]),
+]);
 
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
